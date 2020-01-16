a845517e2e3890c54f9f85ba8180b7f12a7bbf40
Pedro Lourenco <dev.pedrolourenco@gmail.com>
2020-01-16 11:22:27 -0300

    att

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
+
 
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition( 
+      (position) => {
+        const {latitude, longitude} = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+
+        console.log (position)
+
+      }, 
+      (err) => {
+        console.log (err)
+      },
+      {
+        timeout: 30000,
+      }
+      )
+  }, []);
+
+  
   return (
    <div id="app">
      <aside>
@@ -13,26 +44,51 @@ function App() {
 
        <form>
 
-         <div class="input-block">
+         <div className="input-block">
             <label htmlFor="github_username">Usuario do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input
+
+            name="github_username" 
+            id="github_username" 
+            required
+            value={github_username}
+            onChange={ e => setGithubUsername(e.target.value)}
+            />
          </div>
         
-         <div class="input-block">
+         <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input
+             name="techs"
+              id="techs" 
+              required 
+              value={techs}
+              onChange={ e => setTechs(e.target.value)}
+              />
          </div>
 
         <div className="input-group">
 
-            <div class="input-block">
+            <div className="input-block">
                 <label htmlFor="latitude">Latitude</label>
-                <input name="latitude" id="latitude" required />
+                <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required value={latitude} 
+                onChange={e => setLatitude(e.target.value)}
+                />
             </div>
             
-            <div class="input-block">
+            <div className="input-block">
                 <label htmlFor="longitude">Longitudes</label>
-                <input name="longitude" id="longitude" required />
+                <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+                 />
             </div>
            </div>
            <button type="submit" >Salvar</button>
@@ -41,7 +97,64 @@ function App() {
       
        </form>
      </aside>
-     <main></main>
+     <main>
+       <ul>
+         <li className="dev-item">
+           <header>
+             <img src="https://avatars0.githubusercontent.com/u/55964391?s=460&v=4" alt="Pedro Lourenco"/>
+             <div className="user-info">
+               <strong>Pedro Lourenco</strong>
+               <span>React, ReactJS, Pyhon</span>
+             </div>
+           </header>
+           <p>Mestre dos magos</p>
+           <a href="https://github.com/devpedrolourenco">Acessar perfil no github</a>
+         </li>
+
+         <li className="dev-item">
+           <header>
+             <img src="https://avatars0.githubusercontent.com/u/55964391?s=460&v=4" alt="Pedro Lourenco"/>
+             <div className="user-info">
+               <strong>Pedro Lourenco</strong>
+               <span>React, ReactJS, Pyhon</span>
+             </div>
+           </header>
+           <p>Mestre dos magos</p>
+           <a href="https://github.com/devpedrolourenco">Acessar perfil no github</a>
+         </li>
+
+         <li className="dev-item">
+           <header>
+             <img src="https://avatars0.githubusercontent.com/u/55964391?s=460&v=4" alt="Pedro Lourenco"/>
+             <div className="user-info">
+               <strong>Pedro Lourenco</strong>
+               <span>React, ReactJS, Pyhon</span>
+             </div>
+           </header>
+           <p>Mestre dos magos</p>
+           <a href="https://github.com/devpedrolourenco">Acessar perfil no github</a>
+         </li>
+
+         <li className="dev-item">
+           <header>
+             <img src="https://avatars0.githubusercontent.com/u/55964391?s=460&v=4" alt="Pedro Lourenco"/>
+             <div className="user-info">
+               <strong>Pedro Lourenco</strong>
+               <span>React, ReactJS, Pyhon</span>
+             </div>
+           </header>
+           <p>Mestre dos magos</p>
+           <a href="https://github.com/devpedrolourenco">Acessar perfil no github</a>
+         </li>
+
+
+
+
+       </ul>
+
+
+
+     </main>
     
    </div>
   );
